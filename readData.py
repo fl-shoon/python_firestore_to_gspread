28587cf7dd621e4be22d3e56b0faa39eac1388d0
fl-shoon <113501499+fl-shoon@users.noreply.github.com>
2022-09-28 12:49:13 +0900

    Update readData.py

--- readData.py
+++ readData.py
@@ -5,14 +5,14 @@ from firebase_admin import firestore
 import csv, pandas as pd
 
 # Use a service account.
-cred = credentials.Certificate('kostritzer-10dce-915ac49343a8.json')
+cred = credentials.Certificate('your_firebase_admin_key_file.json')
 
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-group_a_ref = db.collection(u'groups').document(u'81T1vrpykrEBZlO9zPcl')
-docs = group_a_ref.collection(u'members').stream()
+group_a_ref = db.collection(u'firestore_collection').document(u'firestore_document')
+docs = group_a_ref.collection(u'firestore_sub_collection').stream()
 
 collection_list = []
 
@@ -21,35 +21,24 @@ for doc in docs:
 
 collection_df = pd.DataFrame(collection_list)
 collection_df.index += 1
-# if collection_df['duties'][1][0] == "OGSxCCJNITucPPEcZzjn":
-#     collection_df['duties'][1][0] = collection_df['duties'][1][0].replace("OGSxCCJNITucPPEcZzjn", "エンジニア")
-
-# if docs.exists:
-    # with open('mycsvfile.csv', 'w', encoding="utf-8") as f: 
-    #     w = csv.DictWriter(f, my_dict.keys())
-    #     w.writeheader()
-    #     w.writerow(my_dict)
-# else:
-    # print(u'No such document!')
 
 for i in range(1,len(collection_df)):
-    if type(collection_df['duties'][i]) == list:
-        for column in collection_df['duties'][i]:  
-            column_index = collection_df['duties'][i].index(column)
+    if type(collection_df['dataframe_column_name'][i]) == list:
+        for column in collection_df['dataframe_column_name'][i]:  
+            column_index = collection_df['dataframe_column_name'][i].index(column)
             if column == "Au5xR4YD8mfoMLsZO6it":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("Au5xR4YD8mfoMLsZO6it", "ディレクター")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("Au5xR4YD8mfoMLsZO6it", "ディレクター")
             elif column == "EmHW1I8aOJv3ef5l9Why":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("EmHW1I8aOJv3ef5l9Why", "ドメイン")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("EmHW1I8aOJv3ef5l9Why", "ドメイン")
             elif column == "NTHF1EzrIjY348IUWD7d":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("NTHF1EzrIjY348IUWD7d", "オンサイト")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("NTHF1EzrIjY348IUWD7d", "オンサイト")
             elif column == "OGSxCCJNITucPPEcZzjn":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("OGSxCCJNITucPPEcZzjn", "エンジニア")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("OGSxCCJNITucPPEcZzjn", "エンジニア")
             elif column == "phfRDnUwo2ohlQs6vRbg":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("phfRDnUwo2ohlQs6vRbg", "その他サービ")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("phfRDnUwo2ohlQs6vRbg", "その他サービ")
             elif column == "zG7xjdK2GvyMuhJSlNEL":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("zG7xjdK2GvyMuhJSlNEL", "デザイナー")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("zG7xjdK2GvyMuhJSlNEL", "デザイナー")
             elif column == "zwAyUruge91v7lS1GPjB":
-                collection_df['duties'][i][column_index] = collection_df['duties'][i][column_index].replace("zwAyUruge91v7lS1GPjB", "サーバー")
+                collection_df['dataframe_column_name'][i][column_index] = collection_df['duties'][i][column_index].replace("zwAyUruge91v7lS1GPjB", "サーバー")
 
-# collection_df.to_csv('mydutiescsvfile.csv', encoding='utf-8')
 collection_df.to_csv('mycsvfile.csv', encoding='utf-8')
